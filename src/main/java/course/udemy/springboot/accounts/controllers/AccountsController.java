9af52832c0daf3be6f48c11ba7aeea245e078adc
Joao Vitor <joaovitormoraveis@gmail.com>
2024-10-16 18:46:26 -0400

    Creating entities

--- src/main/java/course/udemy/springboot/accounts/controllers/AccountsController.java
+++ src/main/java/course/udemy/springboot/accounts/controllers/AccountsController.java
@@ -1,4 +1,4 @@
-package course.udemy.springboot.accounts;
+package course.udemy.springboot.accounts.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/course/udemy/springboot/accounts/repositories/CustomerRepository.java
@@ -0,0 +1,4 @@
+package course.udemy.springboot.accounts.repositories;
+
+public class CustomerRepository {
+}
