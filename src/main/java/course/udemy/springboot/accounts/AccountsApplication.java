d8dcd1ae79a575b3f2ae7794927c12c2f45f5c40
Joao Vitor <joaovitormoraveis@gmail.com>
2024-10-17 20:09:17 -0400

    enhancing api documentation

--- src/main/java/course/udemy/springboot/accounts/AccountsApplication.java
+++ src/main/java/course/udemy/springboot/accounts/AccountsApplication.java
@@ -1,15 +1,40 @@
 package course.udemy.springboot.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Joao Vitor",
+                        email = "test@test.com",
+                        url = "localhost"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "localhost"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts microservice REST API Documentation",
+                url = "localhost"
+        )
+)
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
