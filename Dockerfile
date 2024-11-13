dab2aa951a30ad2b3187eaabf02ad748e7df8f72
Joao Vitor <joaovitormoraveis@gmail.com>
2024-11-12 21:35:02 -0500

    adds curl dependency on curl;update compose yaml and fix project build;

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM openjdk:17-slim
 
-MAINTAINER joao-vitor-moraveis
+RUN apt-get update && \
+    apt-get install -y curl && \
+    rm -rf /var/lib/apt/lists/*
 
 COPY target/accounts-0.0.1-SNAPSHOT.jar accounts-0.0.1-SNAPSHOT.jar
 
--- src/test/java/course/udemy/springboot/accounts/AccountsApplicationTests.java
+++ src/test/java/course/udemy/springboot/accounts/AccountsApplicationTests.java
@@ -3,9 +3,10 @@ package course.udemy.springboot.accounts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class AccountsApplicationTests {
 
     @Test
