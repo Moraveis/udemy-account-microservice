f01460992387dd78dda16a6fbfc8017b5ff088dd
Joao Vitor <joaovitormoraveis@gmail.com>
2024-11-26 20:56:19 -0500

    small fixes for feign clients

--- src/main/java/course/udemy/springboot/accounts/service/client/CardsFeignClient.java
+++ src/main/java/course/udemy/springboot/accounts/service/client/CardsFeignClient.java
@@ -2,14 +2,15 @@ package course.udemy.springboot.accounts.service.client;
 
 import course.udemy.springboot.accounts.dto.CardDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "cards", fallback = CardsFallback.class)
+@FeignClient(name = "cards", fallback = CardsFallback.class)
 public interface CardsFeignClient {
 
-    @GetMapping("/api/cards/fetch")
+    @GetMapping(value = "/api/cards/fetch", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<CardDto> fetchCardDetails(@RequestHeader("course-correlation-id") String correlationId, @RequestParam String mobileNumber);
 }
