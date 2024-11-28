002cad1c8576ad467048bd95aabe165868d26fbb
Joao Vitor <joaovitormoraveis@gmail.com>
2024-11-27 21:32:34 -0500

    implements rate limit

--- src/main/java/course/udemy/springboot/accounts/controllers/AccountsController.java
+++ src/main/java/course/udemy/springboot/accounts/controllers/AccountsController.java
@@ -6,6 +6,7 @@ import course.udemy.springboot.accounts.dto.CustomerDto;
 import course.udemy.springboot.accounts.dto.ErrorResponseDto;
 import course.udemy.springboot.accounts.dto.ResponseDto;
 import course.udemy.springboot.accounts.service.AccountService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -206,9 +207,14 @@ public class AccountsController {
                     )
             )
     })
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.OK).body("Java 17");
     }
 
     @Operation(
